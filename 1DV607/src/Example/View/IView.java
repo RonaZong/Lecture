4f077a041f8efc23dd4c5a0a9b3519e82f3241e5
Xingrong Zong <xz222bb@student.lnu.se>
2020-10-21 16:06:41 +0200

    Create IView.java

---
+++ 1DV607/src/Example/View/IView.java
@@ -0,0 +1,10 @@
+package Example.View;
+
+public interface IView {
+
+    boolean wantsToPlay();
+    void displayResult(boolean win);
+    void displayWelcome();
+    void displayInstruction();
+    void displayDice(int faceValue);
+}
