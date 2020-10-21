9ba4bf3fb6ef28c1f801d17f5714e4c6ea7952a4
Xingrong Zong <xz222bb@student.lnu.se>
2020-10-21 16:06:21 +0200

    Create Dice.java

---
+++ 1DV607/src/Example/Model/Dice.java
@@ -0,0 +1,18 @@
+package Example.Model;
+
+public class Dice {
+
+    private int value;
+
+    public void roll() {
+        value = (int) (Math.random() * 171717.0) % 6 + 1;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+}
