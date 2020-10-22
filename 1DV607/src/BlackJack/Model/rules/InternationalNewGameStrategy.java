1d0573cfb0133ef4f5b40e10dca4d27f56653456
Xingrong Zong <xz222bb@student.lnu.se>
2020-10-22 17:06:55 +0200

    Create InternationalNewGameStrategy.java

---
+++ 1DV607/src/BlackJack/Model/rules/InternationalNewGameStrategy.java
@@ -0,0 +1,27 @@
+package BlackJack.Model.rules;
+
+
+import BlackJack.Model.Card;
+import BlackJack.Model.Dealer;
+import BlackJack.Model.Deck;
+import BlackJack.Model.Player;
+
+class InternationalNewGameStrategy implements INewGameStrategy{
+    public boolean NewGame(Deck a_deck, Dealer a_dealer, Player a_player) {
+        Card c;
+
+        c = a_deck.GetCard();
+        c.Show(true);
+        a_player.DealCard(c);
+
+        c = a_deck.GetCard();
+        c.Show(true);
+        a_dealer.DealCard(c);
+
+        c = a_deck.GetCard();
+        c.Show(true);
+        a_player.DealCard(c);
+
+        return true;
+    }
+}
